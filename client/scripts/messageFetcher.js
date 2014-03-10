99bd5c30176e7267959c8fa668c904e7b1e5043f
Naman Goel <naman34@gmail.com>
2014-03-10 16:00:39 -0700

    Client-side model for getting messages built.

---
+++ client/scripts/messageFetcher.js
@@ -0,0 +1,36 @@
+var lastUpdated = new Date(0);
+
+$(document).ready(function() {
+
+  // Receives array of messages, puts them in appropriate room, updates 'lastUpdated', and sets 'fetchMessages' to run again after delay.
+  var updateMessages = function(response) {
+    //console.log(response.results);
+    _.each(response.results, function(messageObj) {
+      if(messageObj.roomname === "" || !messageObj.roomname) {
+        messageObj.roomname = "lobby";
+      }
+      if(rooms[messageObj.roomname] === undefined) {
+        rooms[messageObj.roomname] = new Room(messageObj.roomname);
+      }
+      rooms[messageObj.roomname].addMessage(messageObj);
+      if( (new Date(messageObj.lastUpdated)).valueOf() > lastUpdated.valueOf() ) {
+        lastUpdated = new Date(messageObj.lastUpdated);
+      }
+    });
+    setTimeout(fetchMessages, 1000);
+  };
+
+  // Fetches new messages and passes them to updateMessages method.
+  var fetchMessages = function() {
+    $.get('https://api.parse.com/1/classes/chatterbox', updateMessages);
+  };
+
+// ?where=' + encodeURIComponent(JSON.stringify({
+//       lastUpdated: {
+//         $gt: lastUpdated
+//       }
+//     }))
+
+  fetchMessages();
+
+});
